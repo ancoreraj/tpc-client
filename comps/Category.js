b1ed67f7b758d2c076b0a764a6c24ca0cb92db54
ankurpowerplay <ankur.jar123@gmail.com>
2023-02-12 20:25:00 +0530

    coming soon page

--- comps/Category.js
+++ comps/Category.js
@@ -18,12 +18,6 @@ const Category = () => {
                                         <i class="fa fa-laptop icon-bg-1"></i>
                                         <h4>Assignment</h4>
                                     </div>
-                                    {/* <ul class="category-list">
-                                        <li><a href="category.html">Lorem <span>93</span></a></li>
-                                        <li><a href="category.html">Lorem <span>233</span></a></li>
-                                        <li><a href="category.html">Lorem <span>183</span></a></li>
-                                        <li><a href="category.html">Lorem <span>343</span></a></li>
-                                    </ul> */}
                                 </div>
                             </div>
                             <div class="col-lg-3 offset-lg-0 offset-md-1 col-sm-6">
@@ -32,41 +26,25 @@ const Category = () => {
                                         <i class="fa fa-apple icon-bg-2"></i>
                                         <h4>Notes</h4>
                                     </div>
-                                    {/* <ul class="category-list">
-                                        <li><a href="category.html">Lorem <span>393</span></a></li>
-                                        <li><a href="category.html">Lorem <span>23</span></a></li>
-                                        <li><a href="category.html">Lorem <span>13</span></a></li>
-                                        <li><a href="category.html">Lorem<span>43</span></a></li>
-                                    </ul> */}
                                 </div>
                             </div>
                             <div class="col-lg-3 offset-lg-0 offset-md-1 col-sm-6">
                                 <div class="category-block">
                                     <div class="header">
                                         <i class="fa fa-home icon-bg-3"></i>
-                                        <h4>Books</h4>
+                                        <h4>Copies</h4>
                                     </div>
-                                    {/* <ul class="category-list">
-                                        <li><a href="category.html">Lorem <span>93</span></a></li>
-                                        <li><a href="category.html">Lorem <span>23</span></a></li>
-                                        <li><a href="category.html">Lorem <span>83</span></a></li>
-                                        <li><a href="category.html">Lorem <span>33</span></a></li>
-                                    </ul> */}
                                 </div>
                             </div>
                             <div class="col-lg-3 offset-lg-0 offset-md-1 col-sm-6">
-                                <div class="category-block">
-                                    <div class="header">
-                                        <i class="fa fa-shopping-basket icon-bg-4"></i>
-                                        <h4>Others</h4>
+                                <a href="/coming-soon">
+                                    <div class="category-block">
+                                        <div class="header">
+                                            <i class="fa fa-shopping-basket icon-bg-4"></i>
+                                            <h4>Books</h4>
+                                        </div>
                                     </div>
-                                        {/* <ul class="category-list">
-                                            <li><a href="category.html">Lorem <span>53</span></a></li>
-                                            <li><a href="category.html">Lorem <span>212</span></a></li>
-                                            <li><a href="category.html">Lorem <span>133</span></a></li>
-                                            <li><a href="category.html">Lorem <span>143</span></a></li>
-                                        </ul> */}
-                                </div>
+                                </a>
                             </div>
                         </div>
                     </div>
